120d108d293b50ab256b57bb32c4c41c9d021e29
Arif Imeri <arifimeri.11@gmail.com>
2024-12-30 17:14:42 +0100

    updated tailwind.config.js

--- ClickMart/tailwind.config.js
+++ ClickMart/tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+export const content = [
+  './pages/**/*.{html,js}',
+  './components/**/*.{html,js}',
+];
 
